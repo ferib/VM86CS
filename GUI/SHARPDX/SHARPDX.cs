f89a792ba3c3792249235c50b37fec9bd637a9c6
= <lopke123@live.nl>
2020-12-01 00:31:02 +0100

    fixed SHARPDX vga 640x400 resolution

--- GUI/SHARPDX/SHARPDX.cs
+++ GUI/SHARPDX/SHARPDX.cs
@@ -10,9 +10,13 @@ namespace x86CS.GUI.SHARPDX
     {
         private Form UIForm;
         private byte[] Memory;
-        private SharpDX.Direct2D1.Bitmap BackBufferBmp;
-        private int Width = 160 * 16;
-        private int Height = 160 * 8;
+        private SharpDX.Direct2D1.Bitmap BitmapBuffer;
+        //private int Width = 160 * 16;
+        //private int Height = 160 * 8;
+        private int Width = 640;
+        private int Height = 400;
+
+        private int SkipCount = 0;
 
         private WindowRenderTarget DrawTarget;
         private Factory factory;
@@ -36,7 +40,7 @@ namespace x86CS.GUI.SHARPDX
             //Set Rendering properties
             RenderTargetProperties renderProp = new RenderTargetProperties()
             {
-                PixelFormat = new PixelFormat(SharpDX.DXGI.Format.B8G8R8A8_UNorm, AlphaMode.Premultiplied),
+                PixelFormat = new PixelFormat(SharpDX.DXGI.Format.R8G8B8A8_UNorm, AlphaMode.Premultiplied),
             };
 
             //set hwnd target properties (permit to attach Direct2D to window)
@@ -51,7 +55,7 @@ namespace x86CS.GUI.SHARPDX
             DrawTarget = new WindowRenderTarget(factory, renderProp, winProp);
 
             // create Bitmap
-            BackBufferBmp = new SharpDX.Direct2D1.Bitmap(DrawTarget, new Size2(Width, Height), new BitmapProperties(DrawTarget.PixelFormat));
+            BitmapBuffer = new SharpDX.Direct2D1.Bitmap(DrawTarget, new Size2(Width, Height), new BitmapProperties(DrawTarget.PixelFormat));
 
             //Start to draw
             DrawTarget.BeginDraw();
@@ -76,6 +80,13 @@ namespace x86CS.GUI.SHARPDX
 
         public override void Cycle()
         {
+            // lets not fry my cpu
+            SkipCount++;
+            if (SkipCount % 10 == 0)
+                SkipCount = 0;
+            else
+                return;
+
             var fontBuffer = new byte[0x2000];
             var displayBuffer = new byte[0xfa0];
 
@@ -88,30 +99,30 @@ namespace x86CS.GUI.SHARPDX
                 int currChar = displayBuffer[i];
                 int fontOffset = currChar * 32;
                 byte attribute = displayBuffer[i + 1];
-                int y = i / 160 * 16;
+                int y = i / 160 * 16; // height pixels
 
                 Color foreColour = vgaDevice.GetColour(attribute & 0xf);
                 Color backColour = vgaDevice.GetColour((attribute >> 4) & 0xf);
 
                 for (var f = fontOffset; f < fontOffset + 16; f++)
                 {
-                    int x = ((i % 160) / 2) * 8;
+                    int x = ((i % 160) / 2) * 8; // width pixels
 
                     for (var j = 7; j >= 0; j--)
                     {
                         var pixelIndex = Width * 4 * y + x * 4;
                         if (((fontBuffer[f] >> j) & 0x1) != 0)
                         {
-                            Memory[pixelIndex + 0] = foreColour.B;
+                            Memory[pixelIndex + 0] = foreColour.R;
                             Memory[pixelIndex + 1] = foreColour.G;
-                            Memory[pixelIndex + 2] = foreColour.R;
+                            Memory[pixelIndex + 2] = foreColour.B;
                             Memory[pixelIndex + 3] = foreColour.A;
                         }
                         else
                         {
-                            Memory[pixelIndex + 0] = backColour.B;
+                            Memory[pixelIndex + 0] = backColour.R;
                             Memory[pixelIndex + 1] = backColour.G;
-                            Memory[pixelIndex + 2] = backColour.R;
+                            Memory[pixelIndex + 2] = backColour.B;
                             Memory[pixelIndex + 3] = backColour.A;
                         }
                         x++;
@@ -119,13 +130,14 @@ namespace x86CS.GUI.SHARPDX
                     y++;
                 }
             }
+
             // copy buffer to device
-            BackBufferBmp.CopyFromMemory(Memory, Width * 4);
+            BitmapBuffer.CopyFromMemory(Memory, Width * 4);
 
             // Draw bitmap to device
             DrawTarget.BeginDraw();
-            DrawTarget.Clear(new SharpDX.Mathematics.Interop.RawColor4(0f, 0f, 0f, 1f));
-            DrawTarget.DrawBitmap(BackBufferBmp, 1.0f, BitmapInterpolationMode.Linear);
+            DrawTarget.Clear(new SharpDX.Mathematics.Interop.RawColor4(0f, 0f, 1f, 1f));
+            DrawTarget.DrawBitmap(BitmapBuffer, 1.0f, BitmapInterpolationMode.Linear);
             DrawTarget.EndDraw();
         }
     }
